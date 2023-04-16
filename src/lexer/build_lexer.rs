40818636e4f4ea91fc65f707d81c60d33849b3e2
omarkoloyede <omarkoloyede@yelp.com>
2023-04-16 22:29:02 +0100

    Added all arms for the token type available at this point

--- src/lexer/build_lexer.rs
+++ src/lexer/build_lexer.rs
@@ -1,60 +1,172 @@
 use std::error::Error;
 
-use crate::lexer::tokens::{IAssociativity, IToken, Token};
+use crate::lexer::tokens::{IAssociativity, IConstants, IFunctions, IToken, Token};
 
 fn build_lexer(expr: &str) -> Result<std::vec::IntoIter<Token>, Box<dyn Error>> {
     let mut tokens: Vec<Token> = vec![];
+    let mut number = String::new();
+    let mut identifier = String::new();
 
     for char in expr.chars() {
         match char {
-            '^' => tokens.push(Token {
-                token_type: IToken::Pow,
-                associativity: Some(IAssociativity::Right),
-                precedence: Some(4),
-                lexeme: char.to_string(),
-            }),
-            '*' => tokens.push(Token {
-                token_type: IToken::Mul,
-                associativity: Some(IAssociativity::Left),
-                precedence: Some(3),
-                lexeme: char.to_string(),
-            }),
-            '/' => tokens.push(Token {
-                token_type: IToken::Div,
-                associativity: Some(IAssociativity::Left),
-                precedence: Some(3),
-                lexeme: char.to_string(),
-            }),
-            '+' => tokens.push(Token {
-                token_type: IToken::Add,
-                associativity: Some(IAssociativity::Left),
-                precedence: Some(2),
-                lexeme: char.to_string(),
-            }),
-            '-' => tokens.push(Token {
-                token_type: IToken::Sub,
-                associativity: Some(IAssociativity::Left),
-                precedence: Some(2),
-                lexeme: char.to_string(),
-            }),
-            '(' => tokens.push(Token {
-                token_type: IToken::LPar,
-                associativity: None,
-                precedence: None,
-                lexeme: char.to_string(),
-            }),
-            ')' => tokens.push(Token {
-                token_type: IToken::RPar,
-                associativity: None,
-                precedence: None,
-                lexeme: char.to_string(),
-            }),
+            '^' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::Pow,
+                    associativity: Some(IAssociativity::Right),
+                    precedence: Some(4),
+                    lexeme: char.to_string(),
+                });
+            }
+            '*' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::Mul,
+                    associativity: Some(IAssociativity::Left),
+                    precedence: Some(3),
+                    lexeme: char.to_string(),
+                });
+            }
+            '/' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::Div,
+                    associativity: Some(IAssociativity::Left),
+                    precedence: Some(3),
+                    lexeme: char.to_string(),
+                });
+            }
+            '+' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::Add,
+                    associativity: Some(IAssociativity::Left),
+                    precedence: Some(2),
+                    lexeme: char.to_string(),
+                })
+            }
+            '-' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::Sub,
+                    associativity: Some(IAssociativity::Left),
+                    precedence: Some(2),
+                    lexeme: char.to_string(),
+                });
+            }
+            '(' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::LPar,
+                    associativity: None,
+                    precedence: None,
+                    lexeme: char.to_string(),
+                });
+            }
+            ')' => {
+                clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
+                tokens.push(Token {
+                    token_type: IToken::RPar,
+                    associativity: None,
+                    precedence: None,
+                    lexeme: char.to_string(),
+                });
+            }
+            char if char.is_numeric() => {
+                match clear_identifier(&mut identifier) {
+                    Some(token) => tokens.push(token),
+                    None => (),
+                }
+
+                number.push(char);
+            }
+            char if char.is_alphabetic() => {
+                match clear_number(&mut number) {
+                    Some(token) => tokens.push(token),
+                    None => (),
+                }
+
+                identifier.push(char);
+            }
             _ => {
-                eprintln!();
                 return Err(format!("Unidentified character: {}", char).into());
             }
         }
     }
 
+    clear_identifier_or_number(&mut identifier, &mut number, &mut tokens);
+
     Ok(tokens.into_iter())
+}
+
+fn clear_identifier(identifier: &mut String) -> Option<Token> {
+    if !identifier.is_empty() {
+        return None;
+    }
+
+    let token = match identifier.as_str() {
+        "min" => Token {
+            token_type: IToken::Fun(IFunctions::Min),
+            associativity: None,
+            precedence: None,
+            lexeme: identifier.clone(),
+        },
+        "max" => Token {
+            token_type: IToken::Fun(IFunctions::Max),
+            associativity: None,
+            precedence: None,
+            lexeme: identifier.clone(),
+        },
+        "pi" => Token {
+            token_type: IToken::Const(IConstants::Pi),
+            associativity: None,
+            precedence: None,
+            lexeme: identifier.clone(),
+        },
+        _ => panic!("Unidentified identifier: {}", identifier),
+    };
+
+    identifier.clear();
+
+    Some(token)
+}
+
+fn clear_number(number: &mut String) -> Option<Token> {
+    if !number.is_empty() {
+        return None;
+    }
+
+    let token = Token {
+        token_type: IToken::Num,
+        associativity: None,
+        precedence: None,
+        lexeme: number.clone(),
+    };
+
+    number.clear();
+
+    Some(token)
+}
+
+fn clear_identifier_or_number(
+    identifier: &mut String,
+    number: &mut String,
+    tokens: &mut Vec<Token>,
+) {
+    match clear_identifier(identifier) {
+        Some(token) => tokens.push(token),
+        None => (),
+    }
+
+    match clear_number(number) {
+        Some(token) => tokens.push(token),
+        None => (),
+    }
 }
