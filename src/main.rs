2b5296526373019abdb3098382b4adf837a781ba
omarkoloyede <omarkoloyede@yelp.com>
2023-04-16 19:13:31 +0100

    Added command line optionerror handling and extracted expression to be evaluated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, error::Error, process};
+
+const PARSER_OPTION: &str = "eval";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let expr = parse_config(&args).unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        process::exit(1);
+    });
+
+    print!("Expression to be evaluated is: {}", expr);
+}
+
+fn parse_config(args: &Vec<String>) -> Result<&str, Box<dyn Error>> {
+    if args.len() != 3 || args[1] != PARSER_OPTION {
+        eprintln!("Usage: <cargo run>|<./executable> eval \"<expression to be evaluated>\"");
+        return Err("Error: Invalid option provided.".into());
+    }
+
+    Ok(&args[2])
 }
